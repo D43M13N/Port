56c0f87769489a0e06ff73d4d243efc78826b603
D43M13N <D43m13N@outlook.com>
2023-01-25 23:47:00 +0100

    initial commit

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,7 @@
+hier beschrijf ik welke projecten ik heb gemaakt. 
+
+* een verwijzing naar github projecten<br>
+* elk project moet een eigen show pagina hebben.<br>
+* vertel welke tools ik heb gebruikt. zoals termux rasberryPi 4 hosting.<br>
+* ook projecten als verbouwen, gamecomputer bouwen enz.<br>
+* le wagon vouching<br>
