4de91140567b8fa6a7affff5f1ddefe001cf7ddb
D43M13N <D43m13N@outlook.com>
2023-02-02 07:30:46 +0100

    added text to home nl

--- lang/nl/home.php
+++ lang/nl/home.php
@@ -2,8 +2,9 @@
 <?php
 
 return [
-	'introduction' => 'dit is de introductie'
+	'introduction' => 'bedankt voor het bezoeken van deze pagina.'
 
+	'about' => 'de tools die gebruikt zijn om deze website te maken zijn; termux(dit is een linux terminal app), vscode/vim(texteditor), phpmyadmin, github, php/laravel/vue. en dit word gehost door mijn raspberry pi vanaf mijn eigen woonkamer.'
 ];
 
 
